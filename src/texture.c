2d9ac70b186b1b0f4616e4c0e249163b60fa5315
Collin Brill <collin.brill@gmail.com>
2018-11-09 21:04:53 -0800

    better texture

--- src/texture.c
+++ src/texture.c
@@ -6,43 +6,33 @@
 /*   By: damiandavis <damiandavis@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/20 04:19:00 by pbondoer          #+#    #+#             */
-/*   Updated: 2018/11/09 19:49:10 by damiandavis      ###   ########.fr       */
+/*   Updated: 2018/11/09 21:05:30 by damiandavis      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "wolf.h"
 
-static void	free_textures(t_mlx *mlx)
-{
-	int i;
-
-	i = 0;
-	while (i < TEX_MAP_SIZE)
-		del_image(mlx, mlx->tex[i++]);
-}
-
 int			load_textures(t_mlx *mlx)
 {
-	static char	*map[TEX_MAP_SIZE] = { "brick.xpm", "wood.xpm", "metal.xpm",
-		"stone.xpm", "grass.xpm", NULL };
-	t_image		*img;
+	static char	*map[TEX_MAP_SIZE] = { "textures/brick.xpm",
+		"textures/wood.xpm", "textures/metal.xpm", "textures/stone.xpm",
+		"textures/grass.xpm", NULL };
+	static char	*himap[TEX_MAP_SIZE] = { "textures_hi/brick.xpm",
+		"textures_hi/wood.xpm", "textures_hi/metal.xpm",
+		"textures_hi/stone.xpm", "textures_hi/grass.xpm", NULL };
 	int			i;
-	char		*t;
 
 	i = 1;
 	while (i < TEX_MAP_SIZE && map[i - 1])
-	{
-		t = ft_strjoin((HI_RES ? "textures_hi/" : "textures/"), map[i - 1]);
-		if ((img = xpm_image(t, mlx)) == NULL)
+		if (!(mlx->tex[i] = xpm_image((HI_RES ? himap[i - 1] : map[i - 1]), mlx)))
 		{
-			ft_strdel(&t);
-			free_textures(mlx);
+			i = 0;
+			while (i < TEX_MAP_SIZE)
+				del_image(mlx, mlx->tex[i++]);
 			return (0);
 		}
-		ft_strdel(&t);
-		mlx->tex[i] = img;
-		i++;
-	}
+		else
+			i++;
 	mlx->max_tex = i - 1;
 	mlx->tex[0] = NULL;
 	mlx->floor = mlx->tex[2];
